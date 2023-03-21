ae9ef3e61236dc2421e5fecdea1e5e9d533e4ca6
Abhi...!! <Abhi.d3v3lop3r@gmail.com>
2023-03-21 17:01:36 +0530

    Added Login and sign up method

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,23 +9,25 @@ export class RecipeService {
 
     recipeChanged = new Subject<Recipe[]>;
 
-    recipes: Recipe[] = [
-        new Recipe('The Router Test',
-            'This is the test only',
-            'https://imgs.search.brave.com/dE7uP4yyL-330glOyCUqvztRstiYVs5QnOMUMb3omdo/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/dGFzdGVvZmhvbWUu/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDE4LzAxL2V4cHMy/ODgwMF9VRzE0MzM3/N0QxMl8xOF8xYl9S/TVMuanBn',
-            [
-                new Ingredients('Tomotoes', 10),
-                new Ingredients('Tomotoes', 10)
-            ]),
+    recipes: Recipe[] = [];
 
-        new Recipe('The Test 2 ',
-            'This is the test only 2',
-            'https://imgs.search.brave.com/tWhjKGYuLFXd2ffnbwr1IAfCIex6QSb2z14c4KZ1tpE/rs:fit:722:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5H/NEFmckFRTWRBcjRa/OGl1WFR5cFJBSGFF/MyZwaWQ9QXBp',
-            [
-                new Ingredients('Lemon', 12),
-                new Ingredients('Lemon', 12)
-            ])
-    ];
+    // recipes: Recipe[] = [
+    //     new Recipe('The Router Test',
+    //         'This is the test only',
+    //         'https://imgs.search.brave.com/dE7uP4yyL-330glOyCUqvztRstiYVs5QnOMUMb3omdo/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/dGFzdGVvZmhvbWUu/Y29tL3dwLWNvbnRl/bnQvdXBsb2Fkcy8y/MDE4LzAxL2V4cHMy/ODgwMF9VRzE0MzM3/N0QxMl8xOF8xYl9S/TVMuanBn',
+    //         [
+    //             new Ingredients('Tomotoes', 10),
+    //             new Ingredients('Tomotoes', 10)
+    //         ]),
+
+    //     new Recipe('The Test 2 ',
+    //         'This is the test only 2',
+    //         'https://imgs.search.brave.com/tWhjKGYuLFXd2ffnbwr1IAfCIex6QSb2z14c4KZ1tpE/rs:fit:722:225:1/g:ce/aHR0cHM6Ly90c2U0/Lm1tLmJpbmcubmV0/L3RoP2lkPU9JUC5H/NEFmckFRTWRBcjRa/OGl1WFR5cFJBSGFF/MyZwaWQ9QXBp',
+    //         [
+    //             new Ingredients('Lemon', 12),
+    //             new Ingredients('Lemon', 12)
+    //         ])
+    // ];
 
     constructor(private slService: ShoppingListService) {
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, OnDestroy } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService } from '../recipes/recipe.service';
-import { Subscription, map ,tap } from 'rxjs';
+import { Subscription, exhaustMap, map, take, tap } from 'rxjs';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,8 @@ export class DataStorageService implements OnDestroy {
   subFetchData: Subscription;
 
   constructor(private http: HttpClient,
-    private recipeService: RecipeService) { }
+    private recipeService: RecipeService,
+    private authRecipe: AuthService) { }
 
   storeRecipes() {
     const recipe = this.recipeService.getRecipes();
@@ -23,21 +25,27 @@ export class DataStorageService implements OnDestroy {
   }
 
   fetchData() {
-    return this.http.get<Recipe[]>('https://ng-course-recipe-book-c4cfa-default-rtdb.firebaseio.com/recipe.json')
-      .pipe(
-        map(recipes => {
-          return recipes.map(recipe => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
-            };
-          });
-        }),
-        tap(recipes => {
+    return this.authRecipe.user.pipe(take(1),
+      exhaustMap(user => {
+        return this.http.get<Recipe[]>('https://ng-course-recipe-book-c4cfa-default-rtdb.firebaseio.com/recipe.json',
+        {
+          params: new HttpParams().set('auth',user.token)
+        }
+        );
 
-          this.recipeService.setRecipes(recipes);
-        })
-      )
+      }),
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    )
   }
 
   ngOnDestroy(): void {
